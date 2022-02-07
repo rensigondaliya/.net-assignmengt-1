eaced9e2f7bce8f5b5c9c3faae6b0c9d215425f9
rensigondaliya <84862313+rensigondaliya@users.noreply.github.com>
2022-02-07 23:38:23 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+     class Class1
+    {
+        private string _name;
+        private int _age;
+
+        public string Name
+        {
+
+            get {
+                return _name; 
+            }
+            set { 
+                _name = value; 
+            }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set {
+                if (value >= 18 && value <= 65)
+                    _age = value;
+                else
+                    Console.WriteLine("this age is not valid");
+            
+            }
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace assignment1 {
+
+
+    internal class assignment
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 c = new Class1();
+            
+            System.Console.WriteLine("hello");
+            Console.Write("enter your name ");
+              c.Name= Console.ReadLine();
+
+
+            Console.Write("enetr your age ");
+            c.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" hi {0} your age is {1} ",c.Name , c.Age);  
+                
+                
+                }
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
